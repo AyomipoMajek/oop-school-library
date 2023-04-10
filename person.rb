b8b13f7ea86e244783542d5e6cf94faa4c62f0d8
Ayomipo Majek <ayomipomajek@gmail.com>
2023-04-10 18:09:59 +0100

    corrections made according to first review

--- person.rb
+++ person.rb
@@ -1,30 +1,19 @@
 class Person
-  attr_accessor :age
-  attr_reader :id, :name
+  attr_reader :id
+  attr_accessor :name, :age
 
-  def initialize(id, age, name: 'Unknown', parent_permission: true)
-    @id = id
+  def initialize(age, parent_permission: true, name: 'Unknown')
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  def name=(new_name)
-    @name = new_name
-  end
-
-  def age=(new_age)
-    @age = new_age
+  def of_age?
+    @age >= 18
   end
 
   def can_use_services?
-    is_of_age? || @parent_permission
-  end
-
-  private
-
-  # person.rb:22:7: C: Naming/PredicateName: Rename is_of_age? to of_age?.
-  def of_age?
-    @age >= 18
+    of_age? || @parent_permission
   end
 end
