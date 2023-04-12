1d4bd6d19d71ad2d1a24d2df76dfe4cb383b07a4
Ayomipo Majek <ayomipomajek@gmail.com>
2023-04-12 09:53:26 +0100

    changes made according to first review

--- decorator.rb
+++ decorator.rb
@@ -19,6 +19,6 @@ end
 
 class TrimmerDecorator < Decorator
   def correct_name
-    super.strip
+    @nameable.correct_name.length > 10 ? @nameable.correct_name[0..9] : @nameable.correct_name
   end
 end
