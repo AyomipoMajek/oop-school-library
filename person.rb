b6e1bed605b1665b01976c9d10dcdff2e67501c1
Ayomipo Majek <ayomipomajek@gmail.com>
2023-05-22 15:15:35 +0100

    persons class created as required, student class worked on

--- person.rb
+++ person.rb
@@ -1,4 +1,27 @@
 require './nameable'
 
 class Person < Nameable
-  
+  attr_reader :id
+  attr_accessor :name, :age, :rentals
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+    @rentals = []
+  end
+
+  def of_age?
+    @age >= 18
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
+  end
+end
--- student.rb
+++ student.rb
@@ -1,12 +1,2 @@
 require_relative 'person'
 
-class Student < Person
-  def initialize(age, name = 'unknown', grade = 'unknown')
-    super(age, name)
-    @grade = grade
-  end
-
-  def play_hooky
-    '¯(ツ)/¯'
-  end
-end
