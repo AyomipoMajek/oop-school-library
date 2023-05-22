4941bc1a56ac4a391779b0ff2d999cb6d13b64ca
Ayomipo Majek <ayomipomajek@gmail.com>
2023-05-22 15:16:23 +0100

    student class created, teacher class worked on

--- student.rb
+++ student.rb
@@ -1,2 +1,12 @@
 require_relative 'person'
 
+class Student < Person
+  def initialize(age, name = 'unknown', grade = 'unknown')
+    super(age, name)
+    @grade = grade
+  end
+
+  def play_hooky
+    '¯(ツ)/¯'
+  end
+end
