1fbd53b25c1907b4e9e1637877bbbc1e15c1dd89
Ayomipo Majek <ayomipomajek@gmail.com>
2023-05-24 18:32:56 +0100

    book, classroom and rental classes created

--- classroom.rb
+++ classroom.rb
@@ -1,26 +1,3 @@
 class Classroom
-  attr_accessor :label, :students
-
-  def initialize(label)
-    @label = label
-    @students = []
-  end
-
-  def add_student(student)
-    student.classroom = self
-    @students << student
-  end
-end
-
-class Student < Person
-  attr_accessor :classroom
-
-  def initialize(age, parent_permission: true, name: 'Unknown')
-    super(age, parent_permission: parent_permission, name: name)
-    @classroom = nil
-  end
-
-  def belongs_to_classroom?(classroom)
-    @classroom == classroom
-  end
+ 
 end
