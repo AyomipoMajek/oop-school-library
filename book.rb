5b998bd51f3e71ce01fc1b2fdc9e99362f561b49
Ayomipo Majek <ayomipomajek@gmail.com>
2023-05-24 18:33:52 +0100

    classes associations setup completed

--- book.rb
+++ book.rb
@@ -1,5 +1,27 @@
 class Book
- 
+  attr_accessor :title, :author, :rentals
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
+
+  def add_rental(date, person)
+    rental = Rental.new(date, person, self)
+    person.rentals << rental
+    @rentals << rental
+  end
+end
+
+class Rental
+  attr_accessor :date, :person, :book
+
+  def initialize(date, person, book)
+    @date = date
+    @person = person
+    @book = book
+  end
 end
 
 class Person
--- rental.rb
+++ rental.rb
@@ -1,3 +1,35 @@
 class Rental
- 
+  attr_accessor :date, :book, :person
+
+  def initialize(book, person, date)
+    @book = book
+    @person = person
+    @date = date
+    book.rentals << self
+    person.rentals << self
+  end
+end
+
+class Book
+  attr_accessor :title, :author, :rentals
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
+end
+
+class Person
+  attr_accessor :name, :age, :rentals
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @rentals = []
+  end
+
+  def rent_book(book, date)
+    Rental.new(book, self, date)
+  end
 end
